ae9a7669b261eb333fc6be1f869a0247f0fe9f78
Parker Hutchinson <hutchinsonparker@gmail.com>
2018-01-07 14:29:02 -0800

    getting colors, typeography, and grid setup.

--- stereo.codes/src/Stereo.js
+++ stereo.codes/src/Stereo.js
@@ -5,6 +5,9 @@ class Stereo extends Component {
   render() {
     return (
       <div className="stereo">
+        <h1>hello h1</h1>
+        <h2>hello h2</h2>
+        <h3>hello h3</h3>
         <Button />
       </div>
     );
