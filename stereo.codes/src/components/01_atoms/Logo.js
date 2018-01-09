db21616b30078bbfa9f004de094580ddf1b7e6cd
Parker Hutchinson <hutchinsonparker@gmail.com>
2018-01-09 12:28:55 -0800

    got the logo done with some css magic. never used the repeating gradient feature in css before. pretty cool and simple.

--- stereo.codes/src/components/01_atoms/Logo.js
+++ stereo.codes/src/components/01_atoms/Logo.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Logo.scss';
 
 const Logo = (props) => (
-    <h1 className={`${props.classes} grid-col-3`}>
+    <h1 className={`${props.classes} grid-col-3 logo`}>
       <a href="#top">Stereo</a>
     </h1>
 )
