bfe535f74d1f858f6b7e880395b197d5871a6a68
Parker Hutchinson <hutchinsonparker@gmail.com>
2018-01-30 11:35:10 -0800

    experimenting with styled-components. thinking of converting my whole scss lib over to it.

--- stereo.codes/src/components/01_atoms/Logo.js
+++ stereo.codes/src/components/01_atoms/Logo.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { showNav } from '../../actions/navigation-actions';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 const Velocity = require('velocity-animate');
 
+const StyledLogo = styled.h1`
+  position: relative;
+  min-height: 50px;
+  line-height: 1.6;
+  font-size: 3.0rem;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  grid-column-start: 2;
+  a {
+    letter-spacing: 6px;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 1.6rem;
+    padding-left: 0;
+    margin-left: 0;
+  }
+`;
 
 class Logo extends Component {
   handleClick(evt) {
@@ -14,9 +32,9 @@ class Logo extends Component {
   }
   render() {
     return(
-      <h1 className={`${this.props.classes} grid-col-3 logo`} onClick={(evt) => this.handleClick(evt)}>
+      <StyledLogo className={`${this.props.classes} grid-col-3 logo`} onClick={(evt) => this.handleClick(evt)}>
         <a href="#top">Stereo</a>
-      </h1>
+      </StyledLogo>
     );
   }
 }
