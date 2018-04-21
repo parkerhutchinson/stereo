ecb2e94368e464cb44f1ab0d7d2fe94fb846b216
Parker Hutchinson <hutchinsonparker@gmail.com>
2018-04-21 11:41:37 -0700

    updated reducer to abstract out some store data for about section. added in a bad bike image.

--- stereo.codes/src/reducers/stereo-reducers.js
+++ stereo.codes/src/reducers/stereo-reducers.js
@@ -5,7 +5,7 @@ import { NAVIGATION_OPEN } from '../actions/navigation-actions';
 import { GRID_ACTIVATE } from '../actions/grid-actions';
 import { SECTION_ADD } from '../actions/observer-actions';
 import { MODAL_OPEN } from '../actions/modal-actions';
-
+import { galleryImages } from '../store/about-store';
 
 function navigation(state = { open: false }, action) {
   switch(action.type) {
@@ -16,14 +16,8 @@ function navigation(state = { open: false }, action) {
   }
 }
 
-function gallery() {
-  const galleryImages = [
-    {image: './images/about/profile-old.jpg'},
-    {image: 'https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?ixlib=rb-0.3.5&s=d5b622e2ea747f0eaa17e73a2040031a'},
-    {image: 'https://images.unsplash.com/photo-1484627147104-f5197bcd6651?auto=format&fit=crop&w=3150&q=80'},
-    {image: 'https://images.unsplash.com/reserve/Hxev8VTsTuOJ27thHQdK_DSC_0068.JPG'},
-  ]
-  return galleryImages;
+const gallery = (state = galleryImages) => {
+  return state;
 }
 
 function grid(state = { activate: false }, action) {
---
+++ stereo.codes/src/store/about-store.js
@@ -0,0 +1,7 @@
+
+export const galleryImages = [
+  {image: './images/about/profile-old.jpg'},
+  {image: './images/about/bike.jpg'},
+  {image: 'https://images.unsplash.com/photo-1484627147104-f5197bcd6651?auto=format&fit=crop&w=3150&q=80'},
+  {image: 'https://images.unsplash.com/reserve/Hxev8VTsTuOJ27thHQdK_DSC_0068.JPG'},
+]
