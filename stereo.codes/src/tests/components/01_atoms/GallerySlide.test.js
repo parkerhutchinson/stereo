d797b620f644d39c443096e1974a7303d1e38c87
Parker Hutchinson <hutchinsonparker@gmail.com>
2018-05-07 17:59:59 -0700

    getting some sinon love in place for some of my outdated test cases. going to go through the app and apply any lingering test cases that i skipped.

--- stereo.codes/src/tests/components/01_atoms/GallerySlide.test.js
+++ stereo.codes/src/tests/components/01_atoms/GallerySlide.test.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import GallerySlide from '../../../components/01_atoms/GallerySlide';
+import sinon from 'sinon';
 
 describe('<GallerySlide />', () => {
   const props = {
     index: 2,
     image: "hello_world.jpg",
   }
+  const spyGetClasses = sinon.spy(GallerySlide.prototype, 'getClasses');
   const wrapper = mount(<GallerySlide image={props.image} currentIndex={props.index}/>);
   it('should render', () => {
     expect(wrapper.exists()).toBe(true);
+  });
+  it('should call getClasses on currentIndex update', () => {
+    wrapper.setProps({ currentIndex: 3 });
+    expect(spyGetClasses.calledOnce).toBe(true);
+    sinon.restore();
+  });
+  it('should have an image path', () => {
+    expect(wrapper.props().image).toEqual(props.image);
+  });
+  it('should have a current index', () => {
+    expect(wrapper.props().currentIndex).toEqual(3);
   });
 });
--- stereo.codes/src/tests/components/01_atoms/NavLink.test.js
+++ stereo.codes/src/tests/components/01_atoms/NavLink.test.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { mount, shallow } from 'enzyme';
 import NavLink from '../../../components/01_atoms/NavLink';
 import { MockProvider } from '../../../lib/MockProvider';
+import sinon from 'sinon';
 
 describe('<NavLink />', () => {
   const props = {
     url: '#test',
     title: 'hello world',
   };
-
+  
+  // const spyScrollTo = sinon.spy(NavLink.prototype, 'handleScrollTo');
   const wrapper = mount(
     <MockProvider>
       <NavLink url={props.url} title={props.title} />
@@ -27,5 +29,11 @@ describe('<NavLink />', () => {
   it('has a href attribute value', () => {
     expect(wrapper.find(NavLink).find('a').prop('href')).toEqual("#test");
   });
+
+  it('should call handleScrollTo when clicked', () => {
+    wrapper.find(NavLink).simulate('click');
+    // expect(spyScrollTo.calledOnce).toBe(true);
+    // sinon.restore();
+  })
 
 });
